8499d47e255f3312a90f6a33abfb3ed33dff9859
David Gaspar-Herrera <david.d.gaspar.h@gmail.com>
2022-08-11 20:11:24 -0500

    Finished 03

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(text, amount) {
+    let string = "";
+    if (amount < 0){
+        string = "ERROR";
+        return string;
+    }
+    for (let i = 0; i < amount; i++){
+        string = string + text;
+    }
+    return string;
 };
-
 // Do not edit below this line
 module.exports = repeatString;

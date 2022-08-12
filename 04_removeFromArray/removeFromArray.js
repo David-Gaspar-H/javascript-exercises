0e907fd51ae8a3c90d12cc62758f54ee3dc935f6
David Gaspar-Herrera <david.d.gaspar.h@gmail.com>
2022-08-11 20:33:06 -0500

    Finished 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr) {
+    for(let j = 1; j < arguments.length; j++){
+        for(let i = 0; i < arr.length; i++){
+            if(arr[i] == arguments[j] && typeof(arr[i]) == typeof(arguments[j])){
+                arr.splice(i,1);
+            }
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line

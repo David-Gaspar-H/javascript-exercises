957f24a2112c582cfe3e42b77d1d6db57688fe2d
David Gaspar-Herrera <david.d.gaspar.h@gmail.com>
2022-08-20 01:43:08 -0500

    finished 08 git push git status

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let sum = 0;
+	for (let i = 0; i < arr.length; i++){
+    sum += arr[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+let total = arr[0]
+for (let i = 1; i < arr.length; i++){
+  total = arr[i] * total;
+}
+return total;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let product = 1;
+	for(i = 1; i <= a; i++){
+    product = product * i;
+  }
+  return product;
 };
 
 // Do not edit below this line

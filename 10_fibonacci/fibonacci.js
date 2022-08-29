879a5bd153795fe99f05b380d5862b2abf42139f
David Gaspar-Herrera <david.d.gaspar.h@gmail.com>
2022-08-29 01:55:41 -0500

    Finished ex 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(position) {
+    let sum = 1;
+    let arr = [1,1];
+    if (position <= 0){
+        return "OOPS"
+    }
+    if (position == 1){
+        return arr[0];
+    }
+    for (let i = 2; i < position; i++){
+        arr[i] = arr[i-1] + arr[i-2];
+    }
+    return arr[position-1];
 };
 
 // Do not edit below this line

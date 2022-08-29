89ac875cb7ae4c95d1fe3b7a95ee7ff1793cc2e3
David Gaspar-Herrera <david.d.gaspar.h@gmail.com>
2022-08-29 01:22:29 -0500

    Finished ex 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string.toLowerCase();
+    let norm = "";
+    let reversed = "";
+    for(let i = 0; i < string.length; i ++){
+        if(string[i].match(/[a-z]/i)){
+            norm += string[i];
+        }
+    }
+    for(let i = string.length - 1; i >= 0; i --){
+        if(string[i].match(/[a-z]/i)){
+            reversed += string[i];
+        }
+    }
+    return (norm == reversed);
 };
 
 // Do not edit below this line

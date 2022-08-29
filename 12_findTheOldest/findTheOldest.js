c1a04c1c59f8bdedb1082ac2b348e3c6fd49e283
David Gaspar-Herrera <david.d.gaspar.h@gmail.com>
2022-08-29 12:31:35 -0500

    Finished ex 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+let sorted = people.sort((a,b) => {
+    if (a.yearOfDeath == undefined){
+        a.yearOfDeath = 2022;
+    }
+    if (b.yearOfDeath == undefined){
+        b.yearOfDeath = 2022;
+    }
+    return a.yearOfDeath - a.yearOfBirth < b.yearOfDeath - b.yearOfBirth ? 1 : -1;
+})
+return sorted[0];
 };
 
 // Do not edit below this line
